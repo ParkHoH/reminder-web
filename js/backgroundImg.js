c69fee417d3197c0f3cec4841789c91977dbb48a
Hohyun Park <ioio6382@gmail.com>
2022-03-15 21:59:39 +0900

    github io 이미지 상대경로 수정

--- js/backgroundImg.js
+++ js/backgroundImg.js
@@ -1,5 +1,5 @@
 const images = ['0.jpg', '1.jpg', '2.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImages = `url('img/${chosenImage}')`;
+const bgImages = `url('./img/${chosenImage}')`;
 document.body.style.backgroundImage = bgImages;
