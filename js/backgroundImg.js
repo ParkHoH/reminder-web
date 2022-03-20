5ca2d6325a4778eb9da5546d5eed35d145443b0e
Hohyun Park <ioio6382@gmail.com>
2022-03-20 20:00:04 +0900

    백그라운드 이미지 적용 방식 변경

--- js/backgroundImg.js
+++ js/backgroundImg.js
@@ -3,5 +3,9 @@
 const images = ['0.jpg', '1.jpg', '2.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImages = `url('./img/${chosenImage}')`;
-document.body.style.backgroundImage = bgImages;
+const bgImageContainer = document.querySelector('.backgroud');
+const bgImage = document.createElement('img');
+
+// bgImage.src = `img/${chosenImage}'`;
+bgImage.src = `img/${chosenImage}`;
+bgImageContainer.appendChild(bgImage);
